cc99f598fce47479625c4c9d117a6e1a034e1f08
DESKTOP-FK0MJUV\JS <kjsj20@naver.com>
2020-10-30 11:51:01 +0900

    노드 데이터베이스 자습 커밋!

--- nodeStudy/server_side_javascript/app_mysql.js
+++ nodeStudy/server_side_javascript/app_mysql.js
@@ -3,12 +3,9 @@ var fs = require('fs');
 
 //mysql 접속.
 var mysql = require("mysql");
-var conn = mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password : "1234",
-    database : "o2"
-});
+var conn = mysql.createConnection(
+    {host: "localhost", user: "root", password: "1234", database: "o2"}
+);
 
 conn.connect();
 
@@ -19,8 +16,9 @@ app.set('view engine', 'pug');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/topic/new', function (req, res) {
-    fs.readdir('data', function (err, files) {
+app.get('/topic/add', function (req, res) {
+    var sql = 'SELECT id,title FROM topic';
+    conn.query(sql, (err, topics, fiedls) => {
         if (err) {
             console.log(err);
             res
@@ -28,67 +26,124 @@ app.get('/topic/new', function (req, res) {
                 .send('Internal Server Error');
         }
 
-        res.render('new', {
-            topics:files
-        });
+        res.render('add', {topics: topics});
     });
 });
 
-app.get(['/topic', '/topic/:id'], function (req, res) {
-    var sql = 'SELECT id,title FROM topic';
-    conn.query(sql, (err, topics, fiedls) => {
-        res.render('view', {
-            topics: topics
-        });
-    });
-    /*fs.readdir('data', function (err, files) {
+app.post('/topic/add', function (req, res) {
+    var title = req.body.title;
+    var description = req.body.description;
+    var author = req.body.author;
+    var sql = "INSERT INTO topic (title, description, author) VALUES(?, ?, ?)"
+    conn.query(sql, [title, description, author], (err, result, fields) => {
         if (err) {
             console.log(err);
             res
                 .status(500)
                 .send('Internal Server Error');
         }
+        res.redirect('/topic/'+result.insertId);
+    });  
+});
+
+app.get([
+     '/topic/:id/edit'
+], function (req, res) {
+    var sql = 'SELECT id,title FROM topic';
+    conn.query(sql, (err, topics, fiedls) => {
         var id = req.params.id;
         if (id) {
-            //id 값이 있을때
-            fs.readFile('data/' + id, 'utf-8', function (err, data) {
+            var sql = 'SELECT * FROM topic WHERE id = ?';
+            conn.query(sql, [id], (err, topic, fields) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send('Internal Server Error');
+                } else {
+                    res.render('edit', {
+                        topics: topics,
+                        topic: topic[0]
+                    });
+                }
+            })
+        } else {
+            console.log("there is no id.");
+            res.status(500).send('Internal Server Error');
+        }
+    });
+});
+
+app.post('/topic/:id/edit', function(req, res){
+    var title = req.body.title;
+    var descritpion = req.body.description;
+    var author = req.body.author;
+    var id = req.params.id;
+    var sql = 'update topic set title=?, description=?, author=? where id =?'
+    conn.query(sql, [title, descritpion, author, id], function(err, result, fields){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        } else{
+            res.redirect('/topic/'+id);
+        }
+    });
+});
+
+app.get('/topic/:id/delete', function(req, res){
+    var sql = 'SELECT id,title FROM topic';
+    var id = req.params.id;
+    conn.query(sql, (err, topics, fiedls) => {
+        var sql = 'select * from topic where id = ?';
+        conn.query(sql, [id], function(err, topic){
+            if(err){
+                console.log(err);
+                res.status(500),send('Internal SErver Error');
+            } else {
+                if(topic.length === 0){
+                    console.log("There is no record.");
+                    res.status(500),send('Internal SErver Error');
+                } else {
+                    res.render('delete', {topics:topics, topic: topic[0]});
+                }
+            }
+        });  
+    });
+});
+
+app.post('/topic/:id/delete', function(req, res){
+    var id = req.params.id;
+    var sql = 'delete from topic where id = ?';
+    conn.query(sql, [id], function(err, result){
+        res.redirect('/topic');
+    });
+});
+
+app.get([
+    '/topic', '/topic/:id'
+], function (req, res) {
+    var sql = 'SELECT id,title FROM topic';
+    conn.query(sql, (err, topics, fiedls) => {
+        var id = req.params.id;
+        if (id) {
+            var sql = 'SELECT * FROM topic WHERE id = ?';
+            conn.query(sql, [id], (err, topic, fields) => {
                 if (err) {
                     console.log(err);
                     res
                         .status(500)
                         .send('Internal Server Error');
+                } else {
+                    res.render('view', {
+                        topics: topics,
+                        topic: topic[0]
+                    });
                 }
-
-                res.render('view', {
-                    topics: files,
-                    title: id,
-                    descirption: data
-                });
-            });
+            })
         } else {
-            // id값이 없을때
-            res.render('view', {
-                topics: files,
-                title: 'Welcome',
-                descirption: 'Hello, JavaScript for Server'
-            });
+            res.render('view', {topics: topics});
         }
-    });*/
-});
-
-app.post('/topic', function (req, res) {
-    var title = req.body.title;
-    var description = req.body.description;
-    fs.writeFile('data/' + title, description, function (err) {
-        if (err) {
-            console.log(err);
-            res
-                .status(500)
-                .send('Internal Server Error');
-        }
-        res.redirect('/topic/'+title);
     });
 });
+
 
 //포트번호를 지정하고 불러옴
 app.listen(3000, function () {
